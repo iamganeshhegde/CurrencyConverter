a49d3dd6103415bb04c71c5a599418ea4a7ed7f5
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-08-18 20:46:30 +0530

    finding missing number from array

--- app/src/main/java/com/example/currencyconverter/HomeActivity.kt
+++ app/src/main/java/com/example/currencyconverter/HomeActivity.kt
@@ -1,11 +1,10 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
-import android.text.SpannableString.valueOf
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-class HomeActivity: AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,24 +20,56 @@ fun main() {
 
 //    smallerNumbersThanCurrent(intArrayOf(8,2,1,1,3))
 
-    mapping()
+//    mapping()
 
+//    println(mySqrt(2147483647).toString())
+
+//    squareroot(8)
+
+    println(numberMiss(intArrayOf(3,4,2,0,1,6)))
+
+}
+
+
+//find missing number from array
+fun missingNumber(nums: IntArray): Int {
+    var sum = 0
+    var i = 0
+    while (i < nums.size) {
+        sum = sum + i - nums[i]
+        i++
+    }
+    return sum + i
+}
+
+
+//println(numberMiss(intArrayOf(3,4,2,0,1,6)))
+fun numberMiss(nums:IntArray): Int {
+    var res: Int = nums.size
+    println("res $res")
+
+    for (i in 0 until nums.size) {
+        res = res xor i xor nums.get(i) // a^b^b = a
+
+
+        println("res and i -  $res,  $i")
+    }
+    return res
 }
 
 fun mapping() {
 
     var input = "codeleet"
-    var inputArray = intArrayOf(4,5,6,7,0,2,1,3)
-    var arr = arrayOf("a","b","c")
+    var inputArray = intArrayOf(4, 5, 6, 7, 0, 2, 1, 3)
+    var arr = arrayOf("a", "b", "c")
 
     var hashmap = mutableMapOf<Int, String>()
     var string = ""
 
 
-    for(i in 0 until inputArray.size) {
+    for (i in 0 until inputArray.size) {
         hashmap.put(inputArray[i], input.get(i).toString())
     }
-
 
 
     val sortedMap: MutableMap<Int, String> = TreeMap(hashmap)
@@ -58,6 +89,56 @@ fun restoreString(s: String, indices: IntArray): String? {
     val arr = CharArray(s.length)
     for (i in indices.indices) arr[indices[i]] = s[i]
     return String(arr)
+}
+
+//find square root
+fun mySqrt(x: Int): Int {
+
+    var answer = 0
+
+    if (x == 1 || x == 2 || x == 3) {
+        return 1
+    }
+    var abc: Long = 0
+
+    for (abc in 0..(x / 2) + 1) {
+        if (abc * abc == x) {
+            answer = abc
+            return answer
+        } else if (abc * abc > x) {
+            answer = abc - 1
+            return answer
+        }
+    }
+
+    return answer
+
+}
+
+
+fun squareroot(x:Int) {
+
+    var first = 1
+    var last = x
+
+
+    while (first < last) {
+        var mid = (first + (last-first)) /2
+
+        if(mid <= x/mid && (mid+1) > x/(mid+1) ) {
+            println("mid $mid");
+            return
+        } else if( mid > x/mid ){
+            last = mid
+        } else {
+            first = mid+1
+        }
+    }
+
+    println("first $first");
+
+
+
 }
 
 
@@ -84,8 +165,7 @@ fun smallerNumbersThanCurrent(nums: IntArray): IntArray? {
     for (i in nums.indices) {
         if (nums[i] == 0) {
             res[i] = 0
-        }
-        else {
+        } else {
             res[i] = count[nums[i] - 1]
             print("res[i] - $i  - ${res[i]}, ")
         }
