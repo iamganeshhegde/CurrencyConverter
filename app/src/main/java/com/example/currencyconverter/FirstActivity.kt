bb0e78db6ee91e53199358916f05bb964cedecfe
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-08-23 14:02:09 +0530

    update

---
+++ app/src/main/java/com/example/currencyconverter/FirstActivity.kt
@@ -0,0 +1,13 @@
+package com.example.currencyconverter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirstActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_first)
+    }
+}
