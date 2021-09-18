bcef4670ddee5eba84660e8edcb0d85e11ffa576
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-09-18 16:28:55 +0530

    Simple  implementation, string, RXJava, array, recyclerview

--- app/src/main/java/com/example/currencyconverter/adobeInterview/FirstMainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/adobeInterview/FirstMainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.currencyconverter.R
 
+
 class FirstMainActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +19,15 @@ class FirstMainActivity:AppCompatActivity() {
 fun main() {
 
 
-    var intersection = intersection(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4))
+//    var intersection = intersection(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4))
+//
+//    intersection.iterator().forEach {
+//        println(it)
+//    }
 
-    intersection.iterator().forEach {
+
+    var findWords = findWords(arrayOf("Hello"))
+    findWords.iterator().forEach {
         println(it)
     }
 
@@ -47,4 +54,24 @@ fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
 
     return intArray.toIntArray()
 
+}
+
+
+fun findWords(words: Array<String>): Array<String> {
+    val strs = arrayOf("QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM")
+    val results: MutableList<String> = ArrayList<String>()
+    for (word in words) {
+        var count1 = 0
+        var count2 = 0
+        var count3 = 0
+        for (c in word.toCharArray()) {
+            if (strs[0].lowercase().indexOf(c) != -1) count1++
+            if (strs[1].lowercase().indexOf(c) != -1) count2++
+            if (strs[2].lowercase().indexOf(c) != -1) count3++
+        }
+        if (count1 == 0 && count2 == 0 || count1 == 0 && count3 == 0 || count2 == 0 && count3 == 0) results.add(
+            word
+        )
+    }
+    return results.toTypedArray()
 }
