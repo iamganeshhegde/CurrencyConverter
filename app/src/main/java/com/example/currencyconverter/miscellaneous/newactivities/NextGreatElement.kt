cd53d0e23a3ea9c699c16712258cfb80a1f98ecd
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-09-23 00:01:35 +0530

    Simple  implementation, string, RXJava, array, recyclerview

--- app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/NextGreatElement.kt
+++ app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/NextGreatElement.kt
@@ -15,25 +15,32 @@ fun main() {
 
 //    var nextGreaterElement = nextGreaterElement(intArrayOf(4, 1, 2), intArrayOf(1, 3, 4, 2))
 //    var nextGreaterElement = nextGreaterElement(intArrayOf(2,4),intArrayOf(1,2,3,4))
-    var nextGreaterElement = nextGreaterElementt(intArrayOf(2,4),intArrayOf(1,2,3,4))
+//    var nextGreaterElement = nextGreaterElementt(intArrayOf(2,4),intArrayOf(1,2,3,4))
+//
+//    nextGreaterElement?.iterator()?.forEach {
+//        println(it)
+//    }
+//
+//
+//    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+//    var testDate = dateFormat.parse("12/09/2021")
+//
+//
+//    var testCalendar = Calendar.getInstance(TimeZone.getTimeZone("PST"))
+//    testCalendar.time = testDate
+//
+//    val msDiff: Long = Calendar.getInstance().getTimeInMillis() - testCalendar.getTimeInMillis()
+//    val daysDiff: Long = TimeUnit.MILLISECONDS.toDays(msDiff)
+//
+//
+//    println(daysDiff)
 
-    nextGreaterElement?.iterator()?.forEach {
-        println(it)
-    }
+//    var majorityElement = majorityElement(intArrayOf(3, 2, 3))
+//
+//    println(majorityElement)
 
 
-    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-    var testDate = dateFormat.parse("12/09/2021")
-
-
-    var testCalendar = Calendar.getInstance(TimeZone.getTimeZone("PST"))
-    testCalendar.time = testDate
-
-    val msDiff: Long = Calendar.getInstance().getTimeInMillis() - testCalendar.getTimeInMillis()
-    val daysDiff: Long = TimeUnit.MILLISECONDS.toDays(msDiff)
-
-
-    println(daysDiff)
+    major()
 
 
 }
@@ -106,4 +113,66 @@ fun nextGreaterElementt(findNums: IntArray, nums: IntArray): IntArray? {
         findNums[i] = map.getOrDefault(findNums[i], -1)
     }
     return findNums
+}
+
+
+fun majorityElement(nums: IntArray): Int {
+
+    var hashmap = mutableMapOf<Int, Int>()
+    var value = 0
+
+    for(i in 0 until nums.size) {
+
+        if(hashmap.contains(nums[i])) {
+            hashmap.set(nums[i], hashmap.get(nums[i])!!+1)
+        } else {
+            hashmap[nums[i]] = 1
+        }
+
+    }
+
+
+    var returnVal = 0
+    var key = 0
+    for (i in hashmap.entries) {
+        if(i.value > returnVal)  {
+            returnVal = i.value
+            key = i.key
+        }
+    }
+
+    return key
+
+
+}
+
+
+fun major() {
+    var intARray = intArrayOf(3,3,2,2,3,1,3)
+
+    var count =1
+    var answer = intARray[0]
+
+    for (i in 1 until intARray.size) {
+        if(answer != intARray[i]) {
+            count--
+        } else {
+            count++
+        }
+
+        println(count)
+        if(count == 0) {
+            count = 1
+            answer = intARray[i]
+        }
+
+    }
+
+
+
+    println()
+    println(answer)
+
+
+
 }
