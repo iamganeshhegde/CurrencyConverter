6cfd3c593db54a68d77b5de6a91523950174c3a6
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-10-27 19:32:20 +0530

    Android leetcode

--- app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/removeNthNode.kt
+++ app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/removeNthNode.kt
@@ -20,8 +20,14 @@ fun main() {
 
 
 
-    var p = Painting("Red", "Green", "Blue")
-    p.prinT()
+//    var p = Painting("Red", "Green", "Blue")
+//    p.prinT()
+
+    println(countValidWords("Ganesh Hegde      hi"))
+
+
+
+
 }
 fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
 
@@ -103,4 +109,73 @@ fun printspecialElements(arr: Array<String>) {
             println(arr[i])
         }
     }
+}
+
+fun countValidWords(sentence: String): Int {
+
+    var array = sentence.split(" +".toRegex())
+//    var array = sentence.split("\\s".toRegex())
+    var count = 0
+
+    for(word in array) {
+        if(word.contains(".*\\d.*".toRegex())) {
+
+        } else if(word.all { it.isLetter() }) {
+            count++
+        } else {
+            if(word.last().isLetter() && word.first().isLetter()) {
+                count++
+            }
+        }
+    }
+    array.iterator().forEach { println(it) }
+
+    return count
+
+}
+
+fun countValidWordsss(sentence: String): Int {
+    val str = sentence.split(" ").toTypedArray()
+    var res = 0
+    for (s in str) {
+        if (s != "" && isValid(s)) {
+            res++
+        }
+    }
+    return res
+}
+
+fun countValidWordss(sentence: String): Int {
+    val str = sentence.split(" ").toTypedArray()
+    var res = 0
+    for (s in str) {
+        if (s != "" && isValid(s)) {
+            res++
+        }
+    }
+    return res
+}
+
+fun isValid(s: String): Boolean {
+    var h = 0
+    var m = 0
+    val ch = s.toCharArray()
+    for (i in ch.indices) {
+        if (Character.isDigit(ch[i]) || ch[i] == ' ') {
+            return false
+        }
+        if (ch[i] == '-') {
+            h++
+            if (h >= 2) return false else if (i == 0 || i == ch.size - 1 || !Character.isLetter(
+                    ch[i - 1]
+                ) || !Character.isLetter(ch[i + 1])
+            ) return false
+        }
+        if (ch[i] == '!' || ch[i] == '.' || ch[i] == ',') {
+            m++
+            if (m >= 2) return false
+            if (i != ch.size - 1) return false
+        }
+    }
+    return true
 }
