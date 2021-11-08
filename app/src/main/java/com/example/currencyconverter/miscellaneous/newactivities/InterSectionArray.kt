ab3b67f5794143b3fd05234a00587b5113a7e4c3
iamganeshhegde <ganesh.hegde312@gmail.com>
2021-11-08 22:26:39 +0530

    Android leetcode

--- app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/InterSectionArray.kt
+++ app/src/main/java/com/example/currencyconverter/miscellaneous/newactivities/InterSectionArray.kt
@@ -132,3 +132,14 @@ fun array(): IntArray? {
     return null
 
 }
+
+fun rotate(matrix: Array<IntArray>) {
+    val n = matrix.size
+    for (i in 0 until n / 2) for (j in i until n - i - 1) {
+        val tmp = matrix[i][j]
+        matrix[i][j] = matrix[n - j - 1][i]
+        matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+        matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+        matrix[j][n - i - 1] = tmp
+    }
+}
